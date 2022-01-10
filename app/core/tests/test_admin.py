d64475c7c5d060d5654565d4e99066702dbff661
RdotSilva <RyandotSilva@gmail.com>
2022-01-10 18:44:58 +0000

    Add test case for user listing

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -17,3 +17,11 @@ class AdminSiteTests(TestCase):
             password="dCV8271a",
             name="Test Name"
         )
+
+    def test_users_listed(self):
+        """Test that users are listed on user page"""
+        url = reverse('admin:core_user_changelist')
+        res = self.client.get(url)
+
+        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.email)
