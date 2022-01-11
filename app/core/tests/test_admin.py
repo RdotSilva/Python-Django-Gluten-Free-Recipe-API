85e2091e8786682c24def4e70052b25248fd2b80
RdotSilva <RyandotSilva@gmail.com>
2022-01-11 19:53:37 +0000

    Change admin email to unique

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -8,14 +8,14 @@ class AdminSiteTests(TestCase):
     def setUp(self):
         self.client = Client()
         self.admin_user = get_user_model().objects.create_superuser(
-            email="admin@test.com",
-            password="dCV8271a"
+            email='admin@test.com',
+            password='dCV8271a'
         )
         self.client.force_login(self.admin_user)
         self.user = get_user_model().objects.create_user(
-            email="admin@test.com",
-            password="dCV8271a",
-            name="Test Name"
+            email='test@test.com',
+            password='dCV8271a',
+            name='Test User'
         )
 
     def test_users_listed(self):
