893c01bc39214979d3004a4f2892e46ae83db797
RdotSilva <RyandotSilva@gmail.com>
2022-01-17 19:44:58 +0000

    Add test for password length

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -46,3 +46,18 @@ class PublicUserApiTests(TestCase):
         res = self.client.post(CREATE_USER_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_password_too_short(self):
+        """Test that the password is 5 characters or more"""
+        payload = {
+            'email': 'test@test.com',
+            'password': 'pass',
+            'name': 'Test Name'
+        }
+        res = self.client.post(CREATE_USER_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+        user_exists = get_user_model().objects.filter(
+            email=payload['email']
+        ).exists()
+        self.assertFalse(user_exists)
