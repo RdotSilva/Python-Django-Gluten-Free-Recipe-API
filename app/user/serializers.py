bcb29b510b49cfccf7470f34f05eccf609922a42
RdotSilva <RyandotSilva@gmail.com>
2022-01-17 19:51:39 +0000

    Add create function

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -1,5 +1,4 @@
 from django.contrib.auth import get_user_model, authenticate
-from django.utils.translation import ugettext_lazy as _
 
 from rest_framework import serializers
 
@@ -11,3 +10,7 @@ class UserSerializer(serializers.ModelSerializer):
         model = get_user_model()
         fields = ('email', 'password', 'name')
         extra_kwargs = {'password': {'write_only': True, 'min_length': 5}}
+
+    def create(self, validated_data):
+        """Create a new user with encrypted password and return it"""
+        return get_user_model().objects.create_user(**validated_data)
