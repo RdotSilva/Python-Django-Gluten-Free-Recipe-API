b4ed7f6c13bf5deb2d6cc7a8a743e625d8c36213
RdotSilva <RyandotSilva@gmail.com>
2022-01-27 21:46:04 +0000

    Fix indentation

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -18,27 +18,26 @@ class UserSerializer(serializers.ModelSerializer):
 
 
 class AuthTokenSerializer(serializers.Serializer):
-    """Serializer for the user authenticaton object"""
+    """Serializer for the user authentication object"""
     email = serializers.CharField()
     password = serializers.CharField(
         style={'input_type': 'password'},
         trim_whitespace=False
     )
 
+    def validate(self, attrs):
+        """Validate and authenticate the user"""
+        email = attrs.get('email')
+        password = attrs.get('password')
 
-def validate(self, attrs):
-    """Validate and authenticate the user"""
-    email = attrs.get('email')
-    password = attrs.get('password')
+        user = authenticate(
+            request=self.context.get('request'),
+            username=email,
+            password=password
+        )
+        if not user:
+            msg = _('Unable to authenticate with provided credentials')
+            raise serializers.ValidationError(msg, code='authentication')
 
-    user = authenticate(
-        request=self.context.get('request'),
-        username=email,
-        password=password
-    )
-    if not user:
-        msg = _('Unable to authenticate with provided credentials')
-        raise serializers.ValidationError(msg, code='authentication')
-
-    attrs['user'] = user
-    return attrs
+        attrs['user'] = user
+        return attrs
