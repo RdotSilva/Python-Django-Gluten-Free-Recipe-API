2b77973e84ac343910cd0a36bea5d8e4dab60d35
RdotSilva <RyandotSilva@gmail.com>
2022-02-18 21:04:17 +0000

    Add test case for fetching ingredient list

--- app/recipe/tests/test_ingredients_api.py
+++ app/recipe/tests/test_ingredients_api.py
@@ -36,3 +36,15 @@ class PrivateIngredientsAPITests(TestCase):
             'testpass123'
         )
         self.client.force_authenticate(self.user)
+
+    def test_retrieve_ingredient_list(self):
+        """Test retrieving a list of ingredients"""
+        Ingredient.objects.create(user=self.user, name='kale')
+        Ingredient.objects.create(user=self.user, name='Salt')
+
+        res = self.client.get(INGREDIENTS_URL)
+
+        ingredients = Ingredient.objects.all().order_by('-name')
+        serializer = IngredientSerializer(ingredients, many=True)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, serializer.data)
